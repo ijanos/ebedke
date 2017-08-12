276b6ff6eecf95da549ad7fc275979adf0325817
János Illés <ijanos@gmail.com>
2017-08-12 19:41:52 +0200

    burgerking menu: show only todays menu

--- provider/burgerking.py
+++ provider/burgerking.py
@@ -3,14 +3,20 @@ from lxml import html
 
 URL = "http://burgerking.hu/cikkek/friss-hirek/uj-hetkoznapi-burger-king-menu"
 
-def getMenu(_):
+def getMenu(today):
+    day = today.weekday()
     with urllib.request.urlopen(URL) as response:
         r = response.read()
         tree = html.fromstring(r)
         weeklymenu = tree.xpath('//*[@id="tabs-0"]/div[7]/div/p[5]//text()')
         weeklymenu = [item for item in weeklymenu if item.strip() is not '']
-        addbr = lambda s: "<br>" + s if ':' in s else s
-        menu = ' '.join([addbr(i) for i in weeklymenu])
+
+        try:
+            add_separator = lambda s: "||" + s if ':' in s and not 'Hétfő' in s else s
+            menu = ' '.join([add_separator(i) for i in weeklymenu]).split('||')[day]
+        except:
+            menu = '-'
+
         return {
             'name': 'Burger King',
             'url': URL,
