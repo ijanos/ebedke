8c15cc3d120c0f2bc7954273aa9e26a2ed50474c
János Illés <ijanos@gmail.com>
2017-08-13 10:12:36 +0200

    remove unnecessary paths from flask

--- run.py
+++ run.py
@@ -2,12 +2,12 @@ from functools import wraps
 
 from main import getDailyMenu_parallel
 
-from flask import Flask, send_from_directory, jsonify, request
+from flask import Flask, jsonify, request
 from werkzeug.contrib.cache import SimpleCache
 
 cache = SimpleCache()
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 def cached(timeout=5 * 60, key='view/%s'):
     def decorator(f):
@@ -26,18 +26,6 @@ def cached(timeout=5 * 60, key='view/%s'):
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
-
-@app.route('/favicon.png')
-def favicon():
-    return app.send_static_file('favicon.png')
-
-@app.route('/js/<path:path>')
-def send_js(path):
-    return send_from_directory('static/js', path)
-
-@app.route('/css/<path:path>')
-def send_css(path):
-    return send_from_directory('static/css', path)
 
 @app.route('/menu')
 @cached(timeout=15 * 60)
