31d71ee7d765628cb9fb647c374c8be4bda12162
János Illés <ijanos@gmail.com>
2017-09-06 16:38:44 +0200

    better menu recognition for Kompót

--- provider/kompot.py
+++ provider/kompot.py
@@ -1,23 +1,15 @@
-import json
-import urllib.parse
-import urllib.request
 from datetime import datetime
-
-import config
+from provider.utils import get_facebook_posts
 
 FB_PAGE = "https://www.facebook.com/pg/KompotBisztro/posts/"
 FB_ID = "405687736167829"
 
 def getMenu(today):
-    params = urllib.parse.urlencode({"access_token": config.FB_ACCESS_TOKEN})
-    url = f"https://graph.facebook.com/v2.10/{FB_ID}/posts?{params}"
-
-    resp = urllib.request.urlopen(url).read()
-    posts = json.loads(resp)
+    posts = get_facebook_posts(FB_ID)
     parse_date = lambda d: datetime.strptime(d, '%Y-%m-%dT%H:%M:%S%z').date()
     menu = next((p for p in posts['data']
                  if parse_date(p['created_time']) == today.date()
-                 and "menü" in p['message']),
+                 and ("menü" in p['message'] or "étvágyat" in p['message'])),
                 {'message': ''})
 
     menu = ' '.join(filter(lambda s: s[0] is not '#', menu['message'].split())) # remove hashtags
