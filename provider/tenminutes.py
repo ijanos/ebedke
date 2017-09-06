7a5cc3ead5c9308d740d8339c6b3c258ea327899
János Illés <ijanos@gmail.com>
2017-09-06 16:55:15 +0200

    use a smaller image for 10 minutes menu

--- provider/tenminutes.py
+++ provider/tenminutes.py
@@ -1,29 +1,29 @@
 import urllib.request
 from io import BytesIO
-from base64 import b64encode
 from datetime import datetime
-
 from PIL import Image
-
 from provider.utils import create_img
-
-
-WIDTH = 513
-HEIGHT = 284
 
 
 URL = "http://www.10minutes.hu/"
 IMG_PATH = "images/home_1_06.png"
 
 def getMenu(today):
+    menu = ""
     with urllib.request.urlopen(URL + IMG_PATH) as response:
         lastmod = response.getheader("Last-Modified")
         if today.date() == datetime.strptime(lastmod, '%a, %d %b %Y %H:%M:%S %Z').date():
             r = response.read()
             menu_img = Image.open(BytesIO(r))
 
-            a_menu_box = (234, 54, 234 + WIDTH, 54 + HEIGHT)
-            b_menu_box = (234, 452, 234 + WIDTH, 452 + HEIGHT)
+            WIDTH = 526
+            HEIGHT = 171
+            x = 224
+            a_y = 162
+            b_y = 549
+
+            a_menu_box = (x, a_y, x + WIDTH, a_y + HEIGHT)
+            b_menu_box = (x, b_y, x + WIDTH, b_y + HEIGHT)
 
             amenu = menu_img.crop(a_menu_box)
             bmenu = menu_img.crop(b_menu_box)
@@ -32,22 +32,21 @@ def getMenu(today):
             new_im.paste(amenu, (0, 0))
             new_im.paste(bmenu, (WIDTH, 0))
 
-            new_im = new_im.point(lambda i: i < 100 and 255)
-            new_im = new_im.convert('1')
             zoom = 0.6
             new_im = new_im.resize((int(WIDTH * 2 * zoom), int(HEIGHT * zoom)), Image.ANTIALIAS)
+
+            new_im = new_im.point(lambda i: i < 100 and 255)
+            new_im = new_im.convert('1')
 
             f = BytesIO()
             new_im.save(f, format="png", optimize=True, compress_level=9, bits=1)
             menu = create_img(f)
-        else:
-            menu = ""
 
-        return {
-            'name': '10 minutes',
-            'url' : URL,
-            'menu': menu
-        }
+    return {
+        'name': '10 minutes',
+        'url' : URL,
+        'menu': menu
+    }
 
 if __name__ == "__main__":
     print(getMenu(datetime.today()))
