9b7a16df8165126db0ae5d8ee7ff4bd9041ce4f8
János Illés <ijanos@gmail.com>
2017-09-11 11:19:19 +0200

    fix golvonal parser

--- provider/golvonal.py
+++ provider/golvonal.py
@@ -5,17 +5,10 @@ from provider.utils import get_dom
 URL = "http://www.golvonalbisztro.hu/heti-menuajanlat.html"
 
 def getMenu(today):
-    day = today.weekday()
-    weekdays = {
-        0: "HÉTFŐ",
-        1: "KEDD",
-        2: "SZERDA",
-        3: "CSÜTÖRTÖK",
-        4: "PÉNTEK"
-    }
+    day = today.weekday() + 1
     try:
         dom = get_dom(URL)
-        menu = dom.xpath(f'//div[@class="fck"]//h3[contains(node(), "{ weekdays[day] }")]/'
+        menu = dom.xpath(f'//div[@class="fck"]//h3[{ day }]/'
                          'following-sibling::p[position() >= 1 and position() < 3]')
         menu = '<br>'.join(p.text for p in menu)
     except:
