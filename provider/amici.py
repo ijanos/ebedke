021f7d0b356cddcc0ead01304fcc6c239e2729fc
János Illés <ijanos@gmail.com>
2017-10-24 21:48:58 +0200

    use correct day in Amici provider if a day is missing from the menu

--- provider/amici.py
+++ provider/amici.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from provider.utils import get_facebook_posts
+from provider.utils import get_facebook_posts, days_lower
 import re
 
 FB_PAGE = "https://www.facebook.com/pg/amicimieicorvin/posts"
@@ -7,15 +7,17 @@ FB_ID = "1861078894105248"
 
 
 def getMenu(today):
-    day_names = ["hétfő", "kedd", "szerda", "csütörtök", "péntek"]
+    day = today.weekday()
     try:
         posts = get_facebook_posts(FB_ID)
         parse_date = lambda d: datetime.strptime(d, '%Y-%m-%dT%H:%M:%S%z').date()
         weekly_menu = next((p for p in posts
-                            if all(day in p['message'].lower() for day in day_names)
+                            if sum(day in p['message'].lower() for day in days_lower) > 3
                             and parse_date(p['created_time']) > today.date() -  timedelta(days=7)), None)
         menu = weekly_menu['message']
-        menu = re.split("Hétfő:|Kedd:|Szerda:|Csütörtök:|Péntek:", menu)[today.weekday() + 1]
+        menu = re.split('(hétfő|kedd|szerda|csütörtök|péntek):', menu, flags=re.IGNORECASE)
+        menu = dict(zip([str.lower(d) for d in menu[1::2]], menu[2::2]))
+        menu = menu[days_lower[day]]
         menu = '<br>'.join(menu.strip().split('\n'))
     except:
         menu = ''
