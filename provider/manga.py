b6de066a004b69a94568a3ffcf46f452eed57475
János Illés <ijanos@gmail.com>
2017-10-24 22:04:04 +0200

    choose Manga daily many by date instead of position

--- provider/manga.py
+++ provider/manga.py
@@ -5,11 +5,12 @@ from provider.utils import get_dom
 URL = "http://mangacowboy.hu/"
 
 def getMenu(today):
-    day = today.weekday()
     try:
         dom = get_dom(URL)
-        menu = dom.xpath('//*[@id="weekly_menu"]//div[@class="weeklyMenuPreview-content"]')
-        menu = '<br>'.join(menu[day].xpath("p/text()"))
+        date = today.strftime("%Y. %m. %d.")
+        menu = dom.xpath(f'//section[@id="weekly_menu"]/ul/li[.//time[contains(text(), "{ date }")]]'
+                            '//div[@class="weeklyMenuPreview-content"]')
+        menu = '<br>'.join(menu[0].xpath("./p/text()"))
     except:
         menu = ''
 
