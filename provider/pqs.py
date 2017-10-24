0300420168d422ed411c32a84f00b40466288cc6
János Illés <ijanos@gmail.com>
2017-10-24 22:07:15 +0200

    use current date find the right column in PQS menu

--- provider/pqs.py
+++ provider/pqs.py
@@ -4,13 +4,18 @@ from provider.utils import get_dom
 PQS_MENU = "http://pqs.hu/hu/etlapok?e=56"
 
 def getMenu(today):
-    day = today.weekday() + 1
+    today = today.strftime("%Y-%m-%d")
     try:
         dom = get_dom(PQS_MENU)
+        for i, th in enumerate(dom.xpath('//table[@id="menu"]/thead//th')):
+            text = ''.join(th.xpath('text()'))
+            if today in text:
+                print(i, text)
+                column = i + 1
         texts = dom.xpath('//*[@id="menu"]//tr[th[contains(text(),"enü") '
                           'or contains(text(),"őztje") '
                           'or contains(text(),"eves")]]'
-                         f'/following-sibling::tr[1]/td[{ day }]/ul//text()')
+                         f'/following-sibling::tr[1]/td[{ column }]/ul//text()')
         menu = ''.join(texts)
         menu = menu.replace("Választott leves", "")
         menu = menu.replace("\t", "")
