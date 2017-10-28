22249e88c8e79e978812a707f837d478b1b64312
János Illés <ijanos@gmail.com>
2017-10-28 09:55:19 +0200

    simplify Burgerking provider with common utility functions

--- provider/burgerking.py
+++ provider/burgerking.py
@@ -1,27 +1,23 @@
-import urllib.request
-from lxml import html
+from provider.utils import get_dom
 
 URL = "http://burgerking.hu/cikkek/friss-hirek/uj-hetkoznapi-burger-king-menu"
 
 def getMenu(today):
     day = today.weekday()
-    with urllib.request.urlopen(URL) as response:
-        r = response.read()
-        tree = html.fromstring(r)
-        weeklymenu = tree.xpath('//*[@id="tabs-0"]/div[7]/div/p[5]//text()')
+    try:
+        dom = get_dom(URL)
+        weeklymenu = dom.xpath('//*[@id="tabs-0"]/div[7]/div/p[5]//text()')
         weeklymenu = [item.strip() for item in weeklymenu if not item.isspace()]
+        add_separator = lambda s: "||" + s if ':' in s and not 'Hétfő' in s else s
+        menu = ' '.join([add_separator(i) for i in weeklymenu]).split('||')[day]
+    except:
+        menu = ''
 
-        try:
-            add_separator = lambda s: "||" + s if ':' in s and not 'Hétfő' in s else s
-            menu = ' '.join([add_separator(i) for i in weeklymenu]).split('||')[day]
-        except:
-            menu = '-'
-
-        return {
-            'name': 'Burger King',
-            'url': URL,
-            'menu': menu
-        }
+    return {
+        'name': 'Burger King',
+        'url': URL,
+        'menu': menu
+    }
 
 if __name__ == "__main__":
     from datetime import datetime
