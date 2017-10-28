2c5a8205ffd609d9fe78e882f90e6c488cb1d62f
János Illés <ijanos@gmail.com>
2017-10-28 14:12:22 +0200

    use more common code and do not show menu during weekends for Portum

--- provider/portum.py
+++ provider/portum.py
@@ -1,21 +1,19 @@
 from datetime import datetime, timedelta
-from provider.utils import get_facebook_posts
+from provider.utils import get_filtered_fb_post
 
 
 URL = "https://www.facebook.com/PortumCorvin/posts/"
 FB_ID = "728866253985071"
 
 def getMenu(today):
-    posts = get_facebook_posts(FB_ID)
-    parse_date = lambda d: datetime.strptime(d, '%Y-%m-%dT%H:%M:%S%z').date()
-    menu = next((p for p in posts
-                 if parse_date(p['created_time']) > today.date() - timedelta(days=7)
-                 and "főételek" in p['message'].lower()),
-                {'message': ''})
-    menu = menu['message']
-    if "Előételek:" in menu:
-        menu = menu.split("Előételek:")[1].strip()
-    menu = '<br>'.join(i for i in menu.split('\n') if i)
+    menu = ''
+    if today.weekday() < 5:
+        is_this_week = lambda date: datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z').date() > today.date() - timedelta(days=7)
+        menu_filter = lambda post: is_this_week(post['created_time']) and "főételek" in post['message'].lower()
+        menu = get_filtered_fb_post(FB_ID, menu_filter)
+        if "Előételek:" in menu:
+            menu = menu.split("Előételek:")[1].strip()
+        menu = '<br>'.join(i for i in menu.split('\n') if i)
 
     return {
         'name': 'Portum',
