79747bb732fa8490f02dc567b0aca094b84f16ef
János Illés <ijanos@gmail.com>
2017-11-03 16:44:23 +0100

    fix Foodie daily menu if there is no exact string match

--- provider/foodie.py
+++ provider/foodie.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from itertools import dropwhile
 from provider.utils import get_filtered_fb_post
 
 
@@ -11,9 +12,8 @@ def getMenu(today):
         is_today = lambda date: datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z').date() == today.date()
         menu_filter = lambda post: is_today(post['created_time']) and "leveseink" in post['message'].lower()
         dailymenu = get_filtered_fb_post(FB_ID, menu_filter)
-        dailymenu = dailymenu.split("Leveseink:")[1]
         menu = []
-        for line in dailymenu.split('\n'):
+        for line in dropwhile(lambda line: "leveseink" in line.lower(), dailymenu.split('\n')):
             line = line.strip()
             if len(line) > 1 and (line[0] == '-' or line[-1] == ':'):
                 menu.append(line)
