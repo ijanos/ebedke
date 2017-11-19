7223a9ac05825e99beca2895d678c1776dae98ef
János Illés <ijanos@gmail.com>
2017-11-19 17:22:58 +0100

    update Amici provider to always take 3 lines from the weekly menu

--- provider/amici.py
+++ provider/amici.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
-from provider.utils import get_filtered_fb_post, days_lower
-import re
+from itertools import dropwhile, islice
+from provider.utils import get_filtered_fb_post, days_lower, skip_empty_lines
+
 
 FB_PAGE = "https://www.facebook.com/pg/amicimieicorvin/posts"
 FB_ID = "1861078894105248"
@@ -12,10 +13,9 @@ def getMenu(today):
         is_this_week = lambda date: datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z').date() > today.date() - timedelta(days=7)
         menu_filter = lambda post: is_this_week(post['created_time']) and sum(day in post['message'].lower() for day in days_lower) > 3
         menu = get_filtered_fb_post(FB_ID, menu_filter)
-        menu = re.split('(hétfő|kedd|szerda|csütörtök|péntek):', menu, flags=re.IGNORECASE)
-        menu = dict(zip([str.lower(d) for d in menu[1::2]], menu[2::2]))
-        menu = menu[days_lower[day]]
-        menu = '<br>'.join(menu.strip().split('\n'))
+        menu = dropwhile(lambda line: days_lower[day] not in line.lower(), skip_empty_lines(menu.split('\n')))
+        menu = islice(menu, 1, 4)
+        menu = '<br>'.join(menu)
     except:
         menu = ''
 
