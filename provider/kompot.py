2d371518c9cf0fd4979fd673b59350c6a7f37eb9
János Illés <ijanos@gmail.com>
2017-12-29 11:44:45 +0100

    get kompót menu when there is only one menu option

--- provider/kompot.py
+++ provider/kompot.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from itertools import dropwhile, islice
+from itertools import dropwhile
 from provider.utils import get_filtered_fb_post, days_lower, skip_empty_lines
 
 
@@ -18,17 +18,20 @@ def getMenu(today):
         weekly_menu_filter = lambda post: is_this_week(post['created_time']) \
                                 and days_lower[day] in ignore_hashtags(post['message'])
         weekly_menu = get_filtered_fb_post(FB_ID, weekly_menu_filter)
-
         if weekly_menu:
             menu_post = dropwhile(lambda line: days_lower[day] not in line.lower(), skip_empty_lines(weekly_menu.split('\n')))
         else:
             menu_post = get_filtered_fb_post(FB_ID, daily_menu_filter).split('\n')
-
         menu_post = list(menu_post)
+
         for i, line in enumerate(menu_post):
             if "A:" in line:
                 menu = "<br>".join((menu_post[i-1], menu_post[i], menu_post[i+1]))
                 break
+        if menu == '':
+            skipfilter = lambda l:  not any(i in l.lower() for i in ["sütiket", "#", "jó étvágyat", "mai menü"])
+            menu = "<br>".join(filter(skipfilter, menu_post))
+
     except:
         pass
 
