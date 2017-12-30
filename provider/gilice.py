6fb808a26c7b6eacf6181d305142eed7a952eaa8
János Illés <ijanos@gmail.com>
2017-12-30 12:20:45 +0100

    remove unused image transformations from gilice provider

--- provider/gilice.py
+++ provider/gilice.py
@@ -10,34 +10,6 @@ from provider.utils import get_filtered_fb_post, create_img, days_lower, get_fb_
 FB_PAGE = "https://www.facebook.com/pg/gilicekonyha/posts/"
 FB_ID = "910845662306901"
 
-def cutimage(url, day):
-    with urllib.request.urlopen(url) as response:
-        r = response.read()
-        menu_img = Image.open(BytesIO(r))
-
-        WIDTH = 370
-        HEIGHT = 123
-        X = 110
-        Y = 67
-        Y = Y + (HEIGHT * day)
-        dailybox = (X, Y, X + WIDTH, Y + HEIGHT)
-
-        menu = menu_img.crop(dailybox)
-
-        new_im = Image.new('L', (WIDTH, HEIGHT))
-        new_im.paste(menu, (0, 0))
-
-        enhancer = ImageEnhance.Contrast(new_im)
-        new_im = enhancer.enhance(0.85)
-
-        new_im = new_im.point(lambda i: i > 90 and 255)
-        new_im = new_im.resize((WIDTH, floor(HEIGHT * 0.56)), Image.BOX)
-        new_im = new_im.convert('1')
-
-        f = BytesIO()
-        new_im.save(f, format="png", optimize=True, compress_level=9, bits=4)
-        return create_img(f)
-
 def getFBMenu(today):
     day = today.weekday()
     menu = ''
