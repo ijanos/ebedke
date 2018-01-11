bd680eac11c8f1021a1e6e890dac6a65d1fd7c0d
János Illés <ijanos@gmail.com>
2018-01-11 20:11:34 +0100

    use image downloader helper function in 10minutes provider

--- provider/tenminutes.py
+++ provider/tenminutes.py
@@ -1,55 +1,50 @@
-import urllib.request
 from io import BytesIO
 from datetime import datetime, timedelta
 from PIL import Image
-from provider.utils import create_img, ocr_image
+from provider.utils import create_img, ocr_image, get_fresh_image
 
 
 URL = "http://www.10minutes.hu/"
 IMG_PATH = "images/home_1_06.png"
 
 def getMenu(today):
-    menu = ""
-    with urllib.request.urlopen(URL + IMG_PATH) as response:
-        lastmod = response.getheader("Last-Modified")
-        if today.date() == datetime.strptime(lastmod, '%a, %d %b %Y %H:%M:%S %Z').date():
-            r = response.read()
-            menu_img = Image.open(BytesIO(r))
+    image = get_fresh_image(URL + IMG_PATH, today.date())
+    if image:
+        image = Image.open(BytesIO(image))
 
-            WIDTH, HEIGHT = 526, 171
+        WIDTH, HEIGHT = 526, 171
+        x, a_y, b_y = 224, 162, 549
 
-            x = 224
-            a_y = 162
-            b_y = 549
+        a_menu_box = (x, a_y, x + WIDTH, a_y + HEIGHT)
+        b_menu_box = (x, b_y, x + WIDTH, b_y + HEIGHT)
 
-            a_menu_box = (x, a_y, x + WIDTH, a_y + HEIGHT)
-            b_menu_box = (x, b_y, x + WIDTH, b_y + HEIGHT)
+        amenu = image.crop(a_menu_box)
+        bmenu = image.crop(b_menu_box)
 
-            amenu = menu_img.crop(a_menu_box)
-            bmenu = menu_img.crop(b_menu_box)
+        img = Image.new('L', (WIDTH, HEIGHT * 2))
+        img.paste(amenu, (0, 0))
+        img.paste(bmenu, (0, HEIGHT))
 
-            img = Image.new('L', (WIDTH, HEIGHT * 2))
+        img = img.point(lambda i: i < 100 and 255)
+        img = img.convert('1')
+
+        f = BytesIO()
+        img.save(f, format="png", optimize=True, compress_level=9, bits=1)
+        menu = ocr_image(f)
+        if not menu:
+            img = Image.new('L', (WIDTH * 2, HEIGHT))
             img.paste(amenu, (0, 0))
-            img.paste(bmenu, (0, HEIGHT))
-
-            img = img.point(lambda i: i < 100 and 255)
-            img = img.convert('1')
-
+            img.paste(bmenu, (WIDTH, 0))
+            zoom = 0.55
+            img = img.resize((int(WIDTH * 2 * zoom), int(HEIGHT * zoom)), Image.ANTIALIAS)
+            img = img.point(lambda i: i < 100 and 255).convert('1')
             f = BytesIO()
             img.save(f, format="png", optimize=True, compress_level=9, bits=1)
-            menu = ocr_image(f)
-            if not menu:
-                img = Image.new('L', (WIDTH * 2, HEIGHT))
-                img.paste(amenu, (0, 0))
-                img.paste(bmenu, (WIDTH, 0))
-                zoom = 0.55
-                img = img.resize((int(WIDTH * 2 * zoom), int(HEIGHT * zoom)), Image.ANTIALIAS)
-                img = img.point(lambda i: i < 100 and 255).convert('1')
-                f = BytesIO()
-                img.save(f, format="png", optimize=True, compress_level=9, bits=1)
-                menu = create_img(f)
-            else:
-                menu = '<br>'.join(menu.splitlines())
+            menu = create_img(f)
+        else:
+            menu = '<br>'.join(menu.splitlines())
+    else:
+        menu = ""
     return menu
 
 menu = {
