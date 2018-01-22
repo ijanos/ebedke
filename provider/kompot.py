b3cc3dcd1d31ab937a4c12499e13b4d8f2ef06d8
János Illés <ijanos@gmail.com>
2018-01-22 16:55:10 +0100

    it was possible to reference this variable before assignment

--- provider/kompot.py
+++ provider/kompot.py
@@ -23,6 +23,7 @@ def getMenu(today):
         menu_post = get_filtered_fb_post(FB_ID, daily_menu_filter).splitlines()
     menu_post = list(menu_post)
 
+    menu = ''
     for i, line in enumerate(menu_post):
         if "A:" in line:
             menu = "<br>".join((menu_post[i-1], menu_post[i], menu_post[i+1]))
