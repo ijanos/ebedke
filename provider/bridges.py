146566ae7e2d0cc1b25487545ac50bbc77b469f7
János Illés <ijanos@gmail.com>
2018-02-25 22:06:25 +0100

    change Bridges from position based parsing to weekday name lookup

--- provider/bridges.py
+++ provider/bridges.py
@@ -1,22 +1,21 @@
 from datetime import timedelta, datetime
-from provider.utils import get_dom, on_workdays
+from itertools import chain
+from provider.utils import get_dom, on_workdays, pattern_slice, days_lower
 
 
 URL = "http://bridges.hu/#heti-menu"
 
 @on_workdays
 def getMenu(today):
-    day = today.weekday() + 1
     dom = get_dom(URL)
-    weekly_menu = dom.xpath('/html/body//section[@id="heti-menu"]//p')
-    weekly_menu = list(filter(lambda p: len(p.text_content().strip()) > 0, weekly_menu))
-    date = weekly_menu[0].text_content().strip()[0:10]
+    this_year = today.strftime("%Y")
+    date = dom.xpath(f'/html/body//*[@id="heti-menu"]//p[contains(text(), "{ this_year }")]')[0].text_content().strip()[0:10]
     this_week = lambda date: datetime.strptime(date, '%Y.%m.%d').date() > today.date() - timedelta(days=6)
     if this_week(date):
-        menu = weekly_menu[day].text_content()
-        menu = menu.replace('1.', '<br>1.')
-        menu = menu.replace('2.', '<br>2.')
-        menu = menu + '<br>' + weekly_menu[6].text_content()
+        weekly_menu = dom.xpath('/html/body//section[@id="heti-menu"]//*[self::h5 or self::p]//text()')
+        menu = pattern_slice(weekly_menu, [days_lower[today.weekday()]], days_lower + ['desszert'], inclusive=False)
+        dessert = pattern_slice(weekly_menu, ['desszert'], [], inclusive=True)
+        menu = "<br>".join(line.strip() for line in chain(menu, dessert))
     else:
         menu = ''
 
