d773d64d68f4d186c804fdd04df24c929f3fe66d
János Illés <ijanos@gmail.com>
2018-02-25 09:15:32 +0100

    drop whitespace from end of lines in Dagoba provider

--- provider/dagoba.py
+++ provider/dagoba.py
@@ -11,7 +11,7 @@ def get_menu(today):
     many_colons = lambda msg: msg.count(':') > 4
     menu_filter = lambda post: is_today(post['created_time']) and many_colons(post['message'])
     menu = get_filtered_fb_post(FB_ID, menu_filter)
-    drop = lambda l: not (l.endswith(':') or l.endswith('!') or l.endswith('.') or l.endswith(','))
+    drop = lambda l: not l.strip().endswith((':', '!', '.', ','))
     menu = '<br>'.join(skip_empty_lines((filter(drop, menu.splitlines()))))
     return menu
 
