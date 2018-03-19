3d4bc79dafa4dd2d6975fc50d0105d7e4ad0864d
János Illés <ijanos@gmail.com>
2018-03-19 22:13:48 +0100

    add JoAsszony provider

---
+++ provider/joasszony.py
@@ -0,0 +1,25 @@
+from datetime import timedelta
+from provider.utils import get_dom, on_workdays
+import lxml
+
+URL = "http://www.gyorsetterem.hu/hetimenu.php"
+
+@on_workdays
+def getMenu(today):
+    dom = get_dom(URL, force_utf8=True)
+    date = dom.xpath("/html/body//div[@class='maidatum']/text()")
+    date = date.pop().strip() if date else None
+    if date == today.strftime("%Y-%m-%d"):
+        menu = dom.xpath("/html/body//div[@class='napimenu']/p/text()")
+        menu = '<br>'.join(menu)
+    else:
+        menu = ''
+    return menu
+
+menu =  {
+    'name': 'JóAsszony',
+    'url': URL,
+    'get': getMenu,
+    'ttl': timedelta(hours=16),
+    'cards': ['bank']
+}
