c356df41f2a618bcd8ed79164af93bf34c3d9d23
János Illés <ijanos@gmail.com>
2018-03-21 21:25:54 +0100

    remove unused ocr code from kerova provider

--- provider/kerova.py
+++ provider/kerova.py
@@ -8,28 +8,6 @@ from provider.utils import get_fb_post_attached_image, get_filtered_fb_post, on_
 FB_PAGE = "https://www.facebook.com/pg/kerovaetelbar/posts/"
 FB_ID = "582373908553561"
 
-def read_image(today):
-    is_this_week = lambda date: datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z').date() > today.date() - timedelta(days=7)
-    menu_filter = lambda post: is_this_week(post['created_time']) and "heti" in post['message'].lower()
-    image = get_fb_post_attached_image(FB_ID, menu_filter)
-    if image:
-        _, image, _ = Image.open(BytesIO(image)).split()
-        f = BytesIO()
-        image.save(f, format="png", optimize=True)
-        menu = ocr_image(f).splitlines()
-        if not menu:
-            return ""
-
-        day = today.weekday()
-        menu = dropwhile(lambda l: days_lower[day] not in l.lower(), menu)
-        head = next(menu)
-        stopwords = days_lower + ["falatozz", "práter"]
-        menu = takewhile(lambda l: not any(word in l.lower() for word in stopwords), menu)
-        menu = f'{head} {" ".join(menu)}'
-        menu = menu.split(":")[1].strip() if ":" in menu else menu
-        return menu
-    else:
-        return ""
 
 @on_workdays
 def get_menu(today):
