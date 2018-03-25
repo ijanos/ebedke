c3613b37c9dc34f993a096d3ffc4c78730d72452
János Illés <ijanos@gmail.com>
2018-03-25 11:59:37 +0200

    less code duplication, worse readability

--- provider/dagoba.py
+++ provider/dagoba.py
@@ -12,10 +12,8 @@ def get_menu(today):
     menu_filter = lambda post: is_today(post['created_time']) and many_colons(post['message'])
     menu = get_filtered_fb_post(FB_ID, menu_filter).splitlines()
     drop = lambda l: l.strip().endswith((':', '!', '.', ','))
-    if drop(menu[0]):
-        menu.pop(0)
-    if drop(menu[-1]):
-        menu.pop()
+    for i in (n for n in (0, -1) if drop(menu[n])):
+        menu.pop(i)
     menu = '<br>'.join(skip_empty_lines(menu))
     return menu
 
