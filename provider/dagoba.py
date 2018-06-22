15d4060bb58662064916eb2ec934de0d9eda9336
János Illés <ijanos@gmail.com>
2018-06-22 22:02:39 +0200

    tune Dagoba heuristic

--- provider/dagoba.py
+++ provider/dagoba.py
@@ -8,8 +8,8 @@ FB_ID = "1742309292469507"
 @on_workdays
 def get_menu(today):
     is_today = lambda date: datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z').date() == today.date()
-    many_colons = lambda msg: msg.count(':') >= 3
-    menu_filter = lambda post: is_today(post['created_time']) and many_colons(post['message'])
+    menu_heuristic = lambda msg: msg.count(':') + msg.count('+') >= 3
+    menu_filter = lambda post: is_today(post['created_time']) and menu_heuristic(post['message'])
     menu = get_filtered_fb_post(FB_ID, menu_filter).splitlines()
     drop = lambda l: l.strip().endswith((':', '!', '.', ','))
     for i in (n for n in (0, -1) if menu and drop(menu[n])):
