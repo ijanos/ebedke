875b68a0ce517fd8627f8cf68d593d8a4d614f8b
János Illés <ijanos@gmail.com>
2018-06-23 15:16:47 +0200

    bump facebook API version

--- provider/utils.py
+++ provider/utils.py
@@ -14,7 +14,7 @@ import config
 
 
 FB_TOKEN = urlencode({"access_token": config.FB_ACCESS_TOKEN})
-FB_API_ROOT = "https://graph.facebook.com/v2.11"
+FB_API_ROOT = "https://graph.facebook.com/v3.0"
 VISION_API_ROOT = "https://vision.googleapis.com/v1/images:annotate"
 
 
