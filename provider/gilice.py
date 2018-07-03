a947f7ca9e96ff29ce53a43fdb2af8b1e5c7227a
János Illés <ijanos@gmail.com>
2018-07-03 17:45:11 +0200

    simplify Golya provider

--- provider/gilice.py
+++ provider/gilice.py
@@ -1,28 +1,18 @@
 from datetime import timedelta, datetime
 from itertools   import takewhile, dropwhile, islice
 
-from provider.utils import get_filtered_fb_post, days_lower, get_fb_cover_url, skip_empty_lines, on_workdays
+from provider.utils import get_filtered_fb_post, days_lower, get_fb_cover_url, skip_empty_lines, on_workdays, pattern_slice
 
 FB_PAGE = "https://www.facebook.com/pg/gilicekonyha/posts/"
 FB_ID = "910845662306901"
 
 @on_workdays
 def get_menu(today):
-    day = today.weekday()
     is_this_week = lambda date: datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z').date() >= today.date() - timedelta(days=7)
     menu_filter = lambda post: is_this_week(post['created_time']) and "jelmagyarázat" in post['message'].lower()
     menu = get_filtered_fb_post(FB_ID, menu_filter)
-    post_parts = menu.split("HETI MENÜ")
-    if len(post_parts) > 1:
-        weekly_menu = post_parts[1]
-        menu = weekly_menu.strip().split("\n")
-        menu = islice(dropwhile(lambda l: days_lower[day] not in l, menu), 1, None)
-        menu = takewhile(lambda l: not any(day in l for day in days_lower), menu)
-        menu = '<br>'.join(skip_empty_lines(menu))
-    elif menu:
-        menu = f'<a href="{get_fb_cover_url(FB_ID)}">heti menü</a>'
-    else:
-        menu = ''
+    menu = pattern_slice(menu.splitlines(), [days_lower[today.weekday()]], days_lower, inclusive=False)
+    menu = '<br>'.join(skip_empty_lines(menu))
 
     return menu
 
