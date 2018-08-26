54d1fa49d3f8179a0fa314f3bbaa06de14806789
János Illés <ijanos@gmail.com>
2018-08-26 16:24:19 +0200

    update frontend
    -port new UI logic to jquery
    -new css

--- static/js/script.js
+++ static/js/script.js
@@ -1,29 +1,78 @@
-function remove_hidden() {
-  const hidden = new Set(decodeURIComponent(window.location.hash || '=').split('=')[1].split(';'));
-  const sections = document.querySelectorAll("main > section");
-  sections.forEach(function(section) {
-    if (hidden.has(section.querySelector("span.close").dataset.name)) {
-      section.remove();
-      document.querySelector('body > header > a').style.display = "block";
-    }
+var SHOW_SETTINGS = false;
+const SEPARATOR= '-';
+
+function loadstate(cookie) {
+  const settings = decodeURIComponent(cookie);
+  var main = $("main");
+  var element_map = new Map();
+
+  $("main section").each(function() {
+    var id = $(this).data('id');
+    element_map.set(id, $(this).detach());
+  });
+
+  settings.split(SEPARATOR).forEach(i => {
+    const id = i.slice(0,2);
+    const enabled = i.slice(-1) == '1';
+    var element;
+    if (element_map.has(id)) {
+      element = element_map.get(id);
+    } else {
+      return;
+    };
+    if (!enabled) {
+      element.hide(0);
+      element.find("input[type='checkbox']").get(0).checked = false;
+    };
+    main.append(element);
+    element_map.delete(id);
+  });
+
+  element_map.forEach((element, id) => {
+    main.append(element);
   });
 }
 
-function add_close_listeners() {
-  const spans = document.querySelectorAll(".close");
-  spans.forEach(function(element) {
-    element.addEventListener('click', function() {
-      name = this.dataset.name;
-      this.parentElement.parentElement.remove();
-      window.location.hash = `${ window.location.hash || "hide=" }${ name };`
-      document.querySelector('body > header > a').style.display = "block";
-    });
-  });
+function save_state() {
+  var settings = $("main section").map(function() {
+    var id = $(this).data('id');
+    var enabled = $(this).find("input[type='checkbox']").get(0).checked ? 1 : 0;
+    return id + enabled;
+  }).get().join(SEPARATOR);
+
+  settings = encodeURIComponent(settings);
+  document.cookie = "settings=" + settings;
 }
 
-function main() {
-  remove_hidden();
-  add_close_listeners();
-}
 
-main();
+$(document).ready(function() {
+  const settings_cookie = document.cookie.replace(/(?:(?:^|.*;\s*)settings\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+  if (settings_cookie) {
+    loadstate(settings_cookie);
+  }
+});
+
+$("input[type='checkbox']").change(function(){
+  save_state();
+});
+
+$("#settings").click(function(){
+  SHOW_SETTINGS = !SHOW_SETTINGS;
+  if (SHOW_SETTINGS) {
+    $("section").slideDown(350);
+    $(".controls").fadeIn(350);
+  } else {
+    $("section").has("input[type='checkbox']:not(:checked)").slideUp(350);
+    $(".controls").fadeOut(350);
+  };
+});
+
+$("section .controls button").click(function(){
+  const item = $(this).parents("section");
+  if (this.className == "up") {
+    item.insertBefore(item.prev());
+  } else {
+    item.insertAfter(item.next());
+  };
+  save_state();
+});
