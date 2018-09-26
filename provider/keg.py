e37ccb6748a41739616aed5da1384df2c4fa0e7e
János Illés <ijanos@gmail.com>
2018-09-26 12:33:51 +0200

    fix KEG daily menu

--- provider/keg.py
+++ provider/keg.py
@@ -11,8 +11,11 @@ def getMenu(today):
     menu_date = dom.xpath(f"/html/body//div[@id='PAGES_CONTAINER']//span[contains(text(), '{this_year}')]/text()")
     menu_date = menu_date[0].split('-')[0] if len(menu_date) > 0 else -1
     if menu_date is not -1 and dt.strptime(menu_date, "%Y.%m.%d").date() > today.date() - timedelta(days=6):
-        menu = skip_empty_lines([p.text_content() for p in dom.xpath('/html/body//div[@id="comp-jhonyf7y"]//p')])
-        menu = '<br>'.join(pattern_slice(menu, [days_lower[today.weekday()]], days_lower, inclusive=False))
+        menu = list(skip_empty_lines([p.text_content() for p in dom.xpath('/html/body//div[@id="comp-jhonyf7y"]//p')]))
+        if any(days_lower[today.weekday()] in line for line in menu):
+            menu = '<br>'.join(pattern_slice(menu, [days_lower[today.weekday()]], days_lower, inclusive=False))
+        else:
+            menu = '<br>'.join(menu)
         menu.replace('\xa0', ' ')
     else:
         menu = ''
