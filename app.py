9e5e79f891eb75ff8bc5015f169b092ca7e50072
János Illés <ijanos@gmail.com>
2018-11-03 15:15:58 +0100

    remove Amici provider

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ import config
 
 MENU_ORDER = [
     tenminutes,
-    amici,
     tacsko,
     cbacorvin,
     dagoba,
--- provider/__init__.py
+++ provider/__init__.py
@@ -5,7 +5,6 @@ __all__ = [
     "pqs",
     "tenminutes",
     "dezso",
-    "amici",
     "kerova",
     "dagoba",
     "greenhouse",
