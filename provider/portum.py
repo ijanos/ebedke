19deca76aa8a1c1b4cb8271fc6e3c03bcba20b4c
János Illés <ijanos@gmail.com>
2018-11-20 21:13:31 +0100

    follow the changes on Portum

--- provider/portum.py
+++ provider/portum.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from provider.utils import get_filtered_fb_post, on_workdays
+from provider.utils import get_filtered_fb_post, on_workdays, skip_empty_lines
 
 
 URL = "https://www.facebook.com/PortumCorvin/posts/"
@@ -8,11 +8,12 @@ FB_ID = "728866253985071"
 @on_workdays
 def getMenu(today):
     is_this_week = lambda date: datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z').date() > today.date() - timedelta(days=7)
-    many_colons = lambda msg: msg.count(':') > 4
-    menu_filter = lambda post: is_this_week(post['created_time']) and many_colons(post['message'])
+    menu_filter = lambda post: is_this_week(post['created_time']) and \
+        any(word in post['message'].lower() for word in ["lunch menü ", "business lunch", "előételek"])
     menu = get_filtered_fb_post(FB_ID, menu_filter)
-    menu = [line for line in menu.splitlines() if line.strip().startswith('-')]
-    menu = '<br>'.join(menu)
+    drop_words = ["előételek", "főételek", "desszer", "étvágy", "menü", "lunch"]
+    menu = '<br>'.join(skip_empty_lines(filter(lambda l: not any(word in l.lower() for word in drop_words), menu.splitlines())))
+    menu = ''.join(char for char in menu if ord(char) < 500)
 
     return menu
 
