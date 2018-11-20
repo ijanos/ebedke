a1af29c2f48020a9c96f34b01e0c856f25da9d74
János Illés <ijanos@gmail.com>
2018-11-20 19:50:22 +0100

    follow aranytacsko changes

--- provider/tacsko.py
+++ provider/tacsko.py
@@ -8,9 +8,10 @@ FB_ID = "211285456287124"
 @on_workdays
 def get_menu(today):
     is_today = lambda date: datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z').date() == today.date()
-    menu_filter = lambda post: is_today(post['created_time']) and "ajánlat" in post['message'].lower()
+    menu_filter = lambda post: is_today(post['created_time']) and \
+        any(word in post['message'].lower() for word in ["ebédmenü", "ebéd menü", "mai menü", "déli menü", "mai ebéd", "ajánlat"])
     menu = get_filtered_fb_post(FB_ID, menu_filter)
-    drop_words = ["ajánlat", "tacskó"]
+    drop_words = ["ajánlat", "tacskó", "ebéd", "menü"]
     menu = '<br>'.join(skip_empty_lines(filter(lambda l: not any(word in l.lower() for word in drop_words), menu.splitlines())))
     return menu
 
