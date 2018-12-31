d3e35080cd36350274865fc31106b567d2402ce1
János Illés <ijanos@gmail.com>
2018-12-31 18:18:02 +0100

    update kbarcelona provider for better matches

--- provider/kbarcelona.py
+++ provider/kbarcelona.py
@@ -9,9 +9,12 @@ FB_ID = "2454065824618853"
 @on_workdays
 def getMenu(today):
     is_this_week = lambda date: datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z').date() > today.date() - timedelta(days=7)
-    menu_filter = lambda post: is_this_week(post['created_time']) \
-                               and days_lower[today.weekday()] in post['message'].lower() \
-                               or "menü" in post['message'].lower()
+    is_today = lambda date: datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z').date() == today.date()
+
+    menu_filter = lambda post: (is_this_week(post['created_time']) \
+                               and days_lower[today.weekday()] in post['message'].lower()) \
+                               or ("menü" in post['message'].lower() and is_today(post['created_time']))
+
     menu = get_filtered_fb_post(FB_ID, menu_filter)
     menu = pattern_slice(menu.splitlines(), [days_lower[today.weekday()], "mai"], days_lower + ["ár:"])
 
