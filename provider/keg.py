4e9febad664d6b40eed1daeb6316aa9d8cf29040
János Illés <ijanos@gmail.com>
2019-01-07 21:48:19 +0100

    update KEG provider

--- provider/keg.py
+++ provider/keg.py
@@ -8,7 +8,7 @@ URL = "http://www.kegsormuvhaz.hu/menu-1"
 def getMenu(today):
     dom = get_dom(URL)
     this_year = today.strftime("%Y")
-    menu_date = dom.xpath(f"/html/body//div[@id='PAGES_CONTAINER']//span[contains(text(), '{this_year}')]/text()")
+    menu_date = dom.xpath(f"/html/body//span[contains(text(), '{this_year}')]/text()")
     menu_date = menu_date[0].split('-')[0] if len(menu_date) > 0 else -1
     if menu_date is not -1 and dt.strptime(menu_date, "%Y.%m.%d").date() > today.date() - timedelta(days=6):
         menu = list(skip_empty_lines([p.text_content() for p in dom.xpath('/html/body//div[@id="comp-jhonyf7y"]//p')]))
