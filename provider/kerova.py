9a12b39b3d5b6224897c43eb438cb0a3c64172a8
János Illés <ijanos@gmail.com>
2019-01-07 23:19:29 +0100

    update kerova provider

--- provider/kerova.py
+++ provider/kerova.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 from io import BytesIO
 from PIL import Image
-from provider.utils import get_fb_post_attached_image, get_filtered_fb_post, on_workdays, ocr_image, days_lower_ascii, pattern_slice, remove_accents, skip_empty_lines
+from provider.utils import get_fb_post_attached_image, on_workdays, ocr_image, days_lower, pattern_slice, skip_empty_lines
 
 
 FB_PAGE = "https://www.facebook.com/pg/kerovaetelbar/posts/"
@@ -10,15 +10,22 @@ FB_ID = "582373908553561"
 
 @on_workdays
 def get_menu(today):
-    is_this_week = lambda date: datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z').date() > today.date() - timedelta(days=7)
-    contains_days = lambda post: sum(day in post for day in days_lower_ascii) > 2
-    menu_filter = lambda post: is_this_week(post['created_time']) and contains_days(remove_accents(post['message'].lower()))
-    menu = get_filtered_fb_post(FB_ID, menu_filter)
-    menu = menu.replace(':', '\n')
-    m = lambda p: remove_accents(p.lower())
-    menu = pattern_slice(menu.splitlines(), [days_lower_ascii[today.weekday()]], days_lower_ascii, inclusive=False, modifier=m)
-    menu = skip_empty_lines(menu)
-    menu = '<br>'.join(menu)
+    is_this_week = lambda date: datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z').date() >= today.date() - timedelta(days=7)
+    menu_filter = lambda post: is_this_week(post['created_time']) and "heti menü" in post['message'].lower()
+    image = get_fb_post_attached_image(FB_ID, menu_filter)
+    if image:
+        image = Image.open(BytesIO(image)).convert('L')
+        f = BytesIO()
+        image.save(f, format="png", optimize=True)
+        menu = ocr_image(f).splitlines()
+        if not menu:
+            return ""
+
+        menu = pattern_slice(menu, [days_lower[today.weekday()]], days_lower + ['desszert', "890"], inclusive=False)
+        menu = "<br>".join(skip_empty_lines(menu))
+    else:
+        return ""
+
     return menu
 
 menu = {
