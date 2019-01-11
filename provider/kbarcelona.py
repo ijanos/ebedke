7769a29e0b536aa4a70522ba98a28c8be96149eb
János Illés <ijanos@gmail.com>
2019-01-11 20:29:21 +0100

    update kbarcelona parser

--- provider/kbarcelona.py
+++ provider/kbarcelona.py
@@ -11,9 +11,12 @@ def getMenu(today):
     is_this_week = lambda date: datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z').date() > today.date() - timedelta(days=7)
     is_today = lambda date: datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z').date() == today.date()
 
-    menu_filter = lambda post: (is_this_week(post['created_time']) \
-                               and days_lower[today.weekday()] in post['message'].lower()) \
-                               or ("menü" in post['message'].lower() and is_today(post['created_time']))
+    menu_filter = lambda post: (is_this_week(post['created_time'])
+                               and days_lower[today.weekday()] in post['message'].lower()
+                               and "asztalfoglalás" not in post['message'].lower()) \
+                               or ("menü" in post['message'].lower()
+                                    and is_today(post['created_time'])
+                                    and "asztalfoglalás" not in post['message'].lower())
 
     menu = get_filtered_fb_post(FB_ID, menu_filter)
     menu = ''.join(char for char in menu if ord(char) < 500)
