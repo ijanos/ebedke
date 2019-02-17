9e982e718bdee297ff90cdd2c28d188b99e9f07e
János Illés <ijanos@gmail.com>
2019-02-17 21:15:38 +0100

    use caching convenience function

--- provider/kajahu.py
+++ provider/kajahu.py
@@ -1,6 +1,5 @@
 from datetime import timedelta
-import requests
-from provider.utils import on_workdays
+from provider.utils import on_workdays, http_get
 
 URL = "https://www.kajahu.com/etlap/d"
 API = "https://appif.kajahu.com/jdmenu?jlang=hu&jseat=-"
@@ -9,7 +8,7 @@ API = "https://appif.kajahu.com/jdmenu?jlang=hu&jseat=-"
 @on_workdays
 def getMenu(today):
     date = today.strftime("%Y-%m-%d")
-    menujson = requests.get(API).json()
+    menujson = http_get(API).json()
     menu = []
     for day in menujson['jdata']:
         if day['ddate'] == date:
