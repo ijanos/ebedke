1d3fa8a931df801a962a35185214d1ca8f5cb7d4
János Illés <ijanos@gmail.com>
2019-03-01 21:55:18 +0100

    follow input page changes

--- provider/input.py
+++ provider/input.py
@@ -1,18 +1,22 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 from provider.utils import get_dom, on_workdays, skip_empty_lines
 
 FB_PAGE = "https://www.facebook.com/inputbistro/posts"
 FB_ID = "339892963137631"
-URL = "https://www.input.hu/#service-section"
+URL = "https://www.input.hu/?page_id=1981"
 
 @on_workdays
 def get_menu(today):
     dom = get_dom(URL)
-    menus = dom.xpath(f'/html/body//div[@class="_serviceContainer"]')
-    menu = menus[-1].xpath(".//p/text()")
-    menu = list(skip_empty_lines(line for line in menu if "menü" not in line.lower()))
+    date = dom.xpath(f"/html/body//div/h2[contains(text(), {today.year})]/text()")
+    date = date.pop() if date else None
+    date = datetime.strptime(date.split("-")[0], "%Y.%m.%d")
 
-    return menu
+    if today - timedelta(days=7) < date:
+        menu = dom.xpath("/html/body//div[p and ul]//text()")
+    else:
+        menu = []
+    return list(skip_empty_lines(menu))
 
 menu = {
     'name': 'Input bar',
