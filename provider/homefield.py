731e18be04fd22cf54eaa85b585c710dc94c3efd
János Illés <ijanos@gmail.com>
2019-03-01 21:34:47 +0100

    fix homefield provider

--- provider/homefield.py
+++ provider/homefield.py
@@ -7,7 +7,7 @@ FB_ID = "190139087785846"
 
 @on_workdays
 def getMenu(today):
-    is_this_week = lambda date: datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z').date() > today.date() - timedelta(days=6)
+    is_this_week = lambda date: datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z').date() > today.date() - timedelta(days=7)
     menu_filter = lambda post: is_this_week(post['created_time']) and sum(day in post['message'].lower() for day in days_lower) >= 2
     menu = get_filtered_fb_post(FB_ID, menu_filter)
     menu = ''.join(char for char in menu if ord(char) < 500)
