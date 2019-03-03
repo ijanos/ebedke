5eeb348efd0f439796a50407863dbfc8d92c4514
János Illés <ijanos@gmail.com>
2019-03-03 20:07:24 +0100

    load enabled modules only

--- ebedke/ebedke.py
+++ ebedke/ebedke.py
@@ -10,14 +10,17 @@ def load_plugins():
         for entry in direntries:
             if entry.name.endswith('.py') and entry.is_file():
                 module = importlib.import_module(f"plugins.{entry.name[:-3]}")
-                all.append(module.plugin)
-                for group in module.plugin.groups:
-                    groups[group].append(module.plugin)
+                if module.plugin.enabled:
+                    all.append(module.plugin)
+                    for group in module.plugin.groups:
+                        groups[group].append(module.plugin)
     groups["all"] = all
     for pluginlist in groups.values():
         pluginlist.sort(key=lambda plugin: plugin.name)
     return groups
 
+
 if __name__ == "__main__":
     groups = load_plugins()
-    print(groups)
+    for group in groups:
+        print(group, groups[group], "\n")
