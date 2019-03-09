f5354268c05ba7356dcd70015c50292104f244b6
János Illés <ijanos@gmail.com>
2019-03-09 22:26:55 +0100

    remove empty lines from all menu

--- ebedke/utils/text.py
+++ ebedke/utils/text.py
@@ -1,4 +1,3 @@
-
 def normalize_menu(menu):
     text = '\n'.join(line.strip() for line in menu)
     if len(text.strip()) < 16:
@@ -7,9 +6,17 @@ def normalize_menu(menu):
         return []
     if len(text) > 2000:
         text = text[0:2000]
+
     # remove emojis
     text = ''.join(char for char in text if ord(char) < 500)
-    return text.splitlines()
+
+    lines = []
+    for line in text.splitlines():
+        line = line.strip()
+        if len(line) > 2:
+            lines.append(line)
+
+    return lines
 
 def skip_empty_lines(text):
     for line in text:
