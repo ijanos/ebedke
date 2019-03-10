c8daf67f32ef385a21905ca606483c348be246b1
János Illés <ijanos@gmail.com>
2019-03-10 10:04:38 +0100

    better emoji stripping

--- ebedke/utils/text.py
+++ ebedke/utils/text.py
@@ -1,3 +1,5 @@
+from unicodedata import category
+
 def normalize_menu(menu):
     text = '\n'.join(line.strip() for line in menu)
     if len(text.strip()) < 16:
@@ -7,8 +9,9 @@ def normalize_menu(menu):
     if len(text) > 2000:
         text = text[0:2000]
 
-    # remove emojis
-    text = ''.join(char for char in text if ord(char) < 500)
+    # remove emojis, "So" -> Symbol, other
+    # https://en.wikipedia.org/wiki/Template:General_Category_(Unicode)
+    text = ''.join(char for char in text if not category(char).startswith("So"))
 
     lines = []
     for line in text.splitlines():
