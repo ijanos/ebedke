b261d481dfa9a4ae2c8243e316f2bb0a597bc134
János Illés <ijanos@gmail.com>
2019-03-20 21:10:15 +0100

    update Seastars

--- ebedke/plugins/seastars.py
+++ ebedke/plugins/seastars.py
@@ -8,11 +8,11 @@ FB_ID = "271018510265811"
 @on_workdays
 def getMenu(today):
     is_today = lambda date: datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z').date() == today.date()
-    menu_keywords = ["napi menü", days_lower[today.weekday()]]
+    menu_keywords = ["mai menü", "napi menü", days_lower[today.weekday()]]
 
     menu_filter = lambda post: is_today(post['created_time']) and any(word in post['message'].lower() for word in menu_keywords)
     menu = get_filtered_fb_post(FB_ID, menu_filter)
-    drop_words = ["mindenkit", "minden menü", "étlapunk", "csodás", days_lower[today.weekday()]]
+    drop_words = ["11:00", "1590", "mindenkit", "minden menü", "étlapunk", "csodás", days_lower[today.weekday()]]
     menu = (line for line in menu.splitlines() if not any(word in line.lower() for word in drop_words))
 
     return list(skip_empty_lines(menu))
