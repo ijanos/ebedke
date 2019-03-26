5afcc35996fb5390149e0cba92ba67593f9efb1d
János Illés <ijanos@gmail.com>
2019-03-26 20:08:57 +0100

    extend general drop words

--- ebedke/utils/text.py
+++ ebedke/utils/text.py
@@ -5,7 +5,7 @@ def normalize_menu(menu):
     text = '\n'.join(line.strip() for line in menu)
     if len(text.strip()) < 16:
         return []
-    if any(word in text.lower() for word in ("zárva", "ünnep", "nincs menü")):
+    if any(word in text.lower() for word in ("zárva", "ünnep", "nincs menü", "feltöltés")):
         return []
     if len(text) > 2000:
         text = text[0:2000]
