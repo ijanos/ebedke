8da0186b746d9e3a6e225b8ca2accfadf3ae1a53
János Illés <ijanos@gmail.com>
2019-04-12 22:27:12 +0200

    rename Seastars

--- ebedke/plugins/seastars.py
+++ ebedke/plugins/seastars.py
@@ -2,17 +2,16 @@ from datetime import datetime, timedelta
 from ebedke.utils.utils import get_filtered_fb_post, days_lower, skip_empty_lines, on_workdays
 from ebedke.pluginmanager import EbedkePlugin
 
-FB_PAGE = "https://www.facebook.com/pg/Seastarsrestaurant/posts/"
+FB_PAGE = "https://www.facebook.com/Haiphongvietnamesekitchen/"
 FB_ID = "271018510265811"
 
 @on_workdays
 def getMenu(today):
     is_today = lambda date: datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z').date() == today.date()
-    menu_keywords = ["mai menü", "napi menü", days_lower[today.weekday()]]
-
+    menu_keywords = ["mai menü", "napi menü", "napi ebéd", "ebédmenü", "ebéd menü", days_lower[today.weekday()]]
     menu_filter = lambda post: is_today(post['created_time']) and any(word in post['message'].lower() for word in menu_keywords)
     menu = get_filtered_fb_post(FB_ID, menu_filter)
-    drop_words = ["11:00", "1590", "mindenkit", "minden menü", "étlapunk", "csodás", days_lower[today.weekday()]]
+    drop_words = ["11:00", "1590", "mindenkit", "napi ebéd","minden menü", "étlapunk", "csodás",  "foglalj", days_lower[today.weekday()]]
     menu = (line for line in menu.splitlines() if not any(word in line.lower() for word in drop_words))
 
     return list(skip_empty_lines(menu))
@@ -20,7 +19,7 @@ def getMenu(today):
 plugin = EbedkePlugin(
     enabled=True,
     groups=["corvin"],
-    name='Seastars',
+    name='Hai Phong Restaurant',
     id='sst',
     url=FB_PAGE,
     downloader=getMenu,
