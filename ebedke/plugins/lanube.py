c2d261901df9f10645a9ccdc24a21234097d9d50
János Illés <ijanos@gmail.com>
2019-05-13 21:44:11 +0200

    add La Nube plugin

---
+++ ebedke/plugins/lanube.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+from ebedke.utils.utils import get_filtered_fb_post, on_workdays, pattern_slice
+from ebedke.pluginmanager import EbedkePlugin
+
+FB_PAGE = "https://www.facebook.com/pg/lanubecafe/posts"
+FB_ID = "1652899941600571"
+
+def fb_filter(post, today):
+    created = datetime.strptime(post['created_time'], '%Y-%m-%dT%H:%M:%S%z')
+    trigger_words = ["menü", "fogás"]
+    more_than_5_lines = post["message"].count("\n") > 5
+    triggered = any(word in post["message"].lower() for word in trigger_words)
+    this_week = created.date() <= today.date() < created.date() + timedelta(days=7)
+    return this_week and triggered and more_than_5_lines
+
+@on_workdays
+def get_menu(today):
+    fbfilter = lambda post: fb_filter(post, today)
+    menu = get_filtered_fb_post(FB_ID, fbfilter)
+    menu = menu.split('\n')
+    return pattern_slice(menu, ["fogás"], ["following", "plate"])
+
+plugin = EbedkePlugin(
+    enabled=True,
+    name='La Nube',
+    id='nube',
+    url=FB_PAGE,
+    downloader=get_menu,
+    ttl=timedelta(hours=24),
+    cards=[],
+    groups=["moricz"]
+)
