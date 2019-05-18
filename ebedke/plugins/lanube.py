5fc40337924cea476eeba53afe1a5879143ccbcc
János Illés <ijanos@gmail.com>
2019-05-18 14:49:15 +0200

    skip one indirection by not using the lambda

--- ebedke/plugins/lanube.py
+++ ebedke/plugins/lanube.py
@@ -5,18 +5,17 @@ from ebedke.pluginmanager import EbedkePlugin
 FB_PAGE = "https://www.facebook.com/pg/lanubecafe/posts"
 FB_ID = "1652899941600571"
 
-def fb_filter(post, today):
-    created = datetime.strptime(post['created_time'], '%Y-%m-%dT%H:%M:%S%z')
-    trigger_words = ["menü", "fogás"]
-    more_than_5_lines = post["message"].count("\n") > 5
-    triggered = any(word in post["message"].lower() for word in trigger_words)
-    this_week = created.date() <= today.date() < created.date() + timedelta(days=7)
-    return this_week and triggered and more_than_5_lines
-
 @on_workdays
 def get_menu(today):
-    fbfilter = lambda post: fb_filter(post, today)
-    menu = get_filtered_fb_post(FB_ID, fbfilter)
+    def fb_filter(post):
+        created = datetime.strptime(post['created_time'], '%Y-%m-%dT%H:%M:%S%z')
+        trigger_words = ["menü", "fogás"]
+        more_than_5_lines = post["message"].count("\n") > 5
+        triggered = any(word in post["message"].lower() for word in trigger_words)
+        this_week = created.date() <= today.date() < created.date() + timedelta(days=7)
+        return this_week and triggered and more_than_5_lines
+
+    menu = get_filtered_fb_post(FB_ID, fb_filter)
     menu = menu.split('\n')
     return pattern_slice(menu, ["fogás"], ["following", "plate"])
 
