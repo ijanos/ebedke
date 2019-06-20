3c633b852b08e7a6d1277477a19e669748cb4ca4
János Illés <ijanos@gmail.com>
2019-06-20 22:37:05 +0200

    fix indents

--- ebedke/connections.py
+++ ebedke/connections.py
@@ -2,5 +2,5 @@ import redis as pyredis
 from ebedke import settings
 
 redis = pyredis.StrictRedis(host=settings.redis_host,
-                          port=settings.redis_port,
-                          decode_responses=False)
+                            port=settings.redis_port,
+                            decode_responses=False)
