6b974506a38c91ee82169ed2f8d9506b64e48aa4
János Illés <ijanos@gmail.com>
2019-08-01 22:09:37 +0200

    read new 10minutes menu image properly

--- ebedke/plugins/tenminutes.py
+++ ebedke/plugins/tenminutes.py
@@ -1,6 +1,8 @@
 from typing import List
 from io import BytesIO
 from datetime import timedelta
+from datetime import datetime
+from itertools import dropwhile
 from PIL import Image
 from ebedke.utils.utils import ocr_image, on_workdays
 from ebedke.utils.http import get_fresh_image
@@ -10,7 +12,7 @@ URL = "http://www.10minutes.hu/"
 IMG_PATH = "images/home_1_06.png"
 
 @on_workdays
-def getMenu(today):
+def getMenu(today: datetime) -> List[str]:
     menu: List[str] = []
     if today.weekday() == 0:  # Monday
         yesterday = today - timedelta(days=3)
@@ -19,29 +21,17 @@ def getMenu(today):
     yesterday = yesterday.replace(hour=12, minute=25)
     image = get_fresh_image(URL + IMG_PATH, yesterday)
     if image:
-        image = Image.open(BytesIO(image))
-
-        WIDTH, HEIGHT = 526, 171
-        x, a_y, b_y = 224, 162, 549
-
-        a_menu_box = (x, a_y, x + WIDTH, a_y + HEIGHT)
-        b_menu_box = (x, b_y, x + WIDTH, b_y + HEIGHT)
-
-        amenu = image.crop(a_menu_box)
-        bmenu = image.crop(b_menu_box)
-
-        img = Image.new('L', (WIDTH, HEIGHT * 2))
-        img.paste(amenu, (0, 0))
-        img.paste(bmenu, (0, HEIGHT))
-
-        img = img.point(lambda i: i < 100 and 255)
+        img = Image.open(BytesIO(image))
+        img = img.point(lambda i: i < 251 and 255)
         img = img.convert('1')
 
         f = BytesIO()
         img.save(f, format="png", optimize=True, compress_level=9, bits=1)
-        menu = ocr_image(f)
-        if menu:
-            menu = menu.splitlines()
+
+        text = ocr_image(f)
+        if text:
+            menu = text.splitlines()
+            menu = dropwhile(lambda line: "menü" not in line.lower(), menu)
     return menu
 
 plugin = EbedkePlugin(
