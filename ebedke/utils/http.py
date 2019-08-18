962ac237fb3fa03bce2062aeb15b1eaaca3b62a2
János Illés <ijanos@gmail.com>
2019-08-18 19:31:33 +0200

    report proper errors when failing redis connection

--- ebedke/utils/http.py
+++ ebedke/utils/http.py
@@ -1,29 +1,38 @@
+import sys
 import pickle
 from functools import partial
 from datetime import datetime
 from typing import Optional, Dict, Any
 import requests
 from lxml import html
-from ebedke import connections, settings
+from ebedke import settings
+from ebedke.connections import redis, RedisConnectionError
 
-def get(url: str, *, params: Dict[str, Any] = None, verify: bool = True) -> requests.Response:
+def unsafe_get(url: str, *, params: Dict[str, Any] = None, verify: bool = True) -> requests.Response:
     headers = {
         'User-Agent': settings.user_agent,
     }
     req_get = partial(requests.get, headers=headers, params=params, timeout=10, verify=verify)
 
     if settings.debug_mode:
-        pickled_cache = connections.redis.get(f"cache:{url}")
+        pickled_cache = redis.get(f"cache:{url}")
         if not pickled_cache:
             print("[ebedke] saving to redis cache")
             response = req_get(url)
-            connections.redis.set(f"cache:{url}", pickle.dumps(response), ex=3600)
+            redis.set(f"cache:{url}", pickle.dumps(response), ex=3600)
         else:
             print("[ebedke] loaded from redis cache")
             response = pickle.loads(pickled_cache)
     else:
         response = req_get(url)
     return response
+
+def get(url: str, *, params: Dict[str, Any] = None, verify: bool = True) -> requests.Response:
+    try:
+        return unsafe_get(url, params=params, verify=verify)
+    except RedisConnectionError:
+        print("Redis connection error. Exiting.")
+        sys.exit(1)
 
 def get_dom(url: str, force_utf8: bool = False, verify: bool = True) -> html.HtmlElement:
     response = get(url, verify=verify)
