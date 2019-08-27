cf90cb50c2e784c18bff9d30ceb479dbca092de2
János Illés <ijanos@gmail.com>
2019-08-27 21:58:45 +0200

    remove english menu from muzikum

--- ebedke/plugins/muzikum.py
+++ ebedke/plugins/muzikum.py
@@ -1,5 +1,7 @@
 from datetime import timedelta, datetime
 from ebedke.utils.utils import on_workdays
+from ebedke.utils.text import pattern_slice
+from ebedke.utils.date import days_lower
 from ebedke.utils.http import get_dom
 from ebedke.pluginmanager import EbedkePlugin
 
@@ -7,18 +9,16 @@ URL = "http://muzikum.hu/heti-menu/"
 
 @on_workdays
 def getMenu(today):
-    day = today.weekday()
     dom = get_dom(URL)
     date = dom.xpath('//div[@class="content-right"]//h2/text()')
     date = date[0].strip().split('|')[1].strip()[:5]
     date = datetime.strptime(f'{ today.year }.{ date }', '%Y.%m.%d').date()
     if date > today.date() - timedelta(days=7):
-        menu = dom.xpath('//div[@class="content-right"]//div/p[not(span)]')
-        menu = menu[day].text_content().splitlines()
+        menu = dom.xpath('/html/body//div[@class="content-right"]//div/*[self::h3 or self::p]/text()')
     else:
         menu = []
 
-    return menu
+    return pattern_slice(menu, [days_lower[today.weekday()]], days_lower + ["asztalfoglalás", "dolgozóknak"])
 
 
 plugin = EbedkePlugin(
