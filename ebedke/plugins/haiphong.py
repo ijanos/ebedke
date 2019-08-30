7a537c65e4caa3333ebc00e3b3988e3e4f2ca3a9
János Illés <ijanos@gmail.com>
2019-08-30 21:28:10 +0200

    remove more non-menu lines from HaiPhong menu

--- ebedke/plugins/haiphong.py
+++ ebedke/plugins/haiphong.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from ebedke.utils.utils import days_lower, skip_empty_lines, on_workdays
+from ebedke.utils.utils import days_lower, on_workdays
 from ebedke.utils import facebook
 from ebedke.pluginmanager import EbedkePlugin
 
@@ -12,11 +12,11 @@ def getMenu(today):
     menu_keywords = ["mai menü", "napi menü", "napi ebéd", "ebédmenü", "ebéd menü", days_lower[today.weekday()]]
     menu_filter = lambda post: is_today(post['created_time']) and any(word in post['message'].lower() for word in menu_keywords)
     menu = facebook.get_filtered_post(FB_ID, menu_filter)
-    drop_words = ["11:00", "36303366888", "1590", "mindenkit", "napi ebéd", "minden menü",
-                  "étlapunk", "csodás", "foglalj", days_lower[today.weekday()]]
-    menu = (line for line in menu.splitlines() if not any(word in line.lower() for word in drop_words))
+    drop_words = ["11:", "36303366888", "1590", "mindenkit", "napi ebéd", "minden menü",
+                  "étlapunk", "csodás", "foglalj", "házi limonádé", "üveges üdítő", "sörök", days_lower[today.weekday()]]
+    menu = (line.strip("-+ ") for line in menu.splitlines() if not any(word in line.lower() for word in drop_words))
 
-    return list(skip_empty_lines(menu))
+    return menu
 
 plugin = EbedkePlugin(
     enabled=True,
