05ea858dd75a0024d11759fcc613a7ff0e609e06
János Illés <ijanos@gmail.com>
2019-10-06 19:27:31 +0200

    move word lists to its own module

---
+++ ebedke/utils/corpus.py
@@ -0,0 +1,32 @@
+from typing import List
+
+non_food_words: List[str] = [
+    "ajánlatunk",
+    "ajánlat",
+    "irány",
+    "kedves",
+    "kultúrált",
+    "mais",
+    "mai",
+    "nyitva",
+    "rendelj",
+    "wolt",
+    "meglepetés",
+    "menü",
+    "szuper"
+    "terasz",
+    "vendégeink",
+    "glutén",
+    "mentes"
+]
+
+
+closed_words: List[str] = [
+    "zárva",
+    "ünnep",
+    "nincs menü",
+    "feltöltés",
+    "zártkörű",
+    "technikai okok",
+    "nem elérhető"
+]
