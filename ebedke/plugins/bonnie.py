bbe8799a7a1bec85d5ab987fa485824644d36bf7
János Illés <ijanos@gmail.com>
2019-10-12 10:22:58 +0200

    follow changes to Bonnie webpage

--- ebedke/plugins/bonnie.py
+++ ebedke/plugins/bonnie.py
@@ -1,34 +1,28 @@
+import re
 from typing import List
-from io import BytesIO
 from datetime import timedelta
-from datetime import datetime
-from PIL import Image
+from datetime import date, datetime
 from ebedke.pluginmanager import EbedkePlugin
-from ebedke.utils.utils import ocr_image, on_workdays
-from ebedke.utils.http import get_fresh_image
+from ebedke.utils.utils import on_workdays
 from ebedke.utils.text import pattern_slice
 from ebedke.utils.http import get_dom
 from ebedke.utils.date import days_lower
 
-URL_ROOT = "http://bonnierestro.hu"
-URL = f"{URL_ROOT}/hu/napimenu/"
+URL = "http://bonnierestro.hu/heti-menue.html"
 
 @on_workdays
 def get_menu(today: datetime) -> List[str]:
-    menu: List[str] = []
     dom = get_dom(URL)
-    menu_img = dom.xpath("/html/body//img[contains(@src, 'napi')]")
-    img_src = menu_img.pop().get("src")
-    img_url = f"{URL_ROOT}{img_src}"
-    image = get_fresh_image(img_url, today - timedelta(days=6))
-    if image:
-        img = Image.open(BytesIO(image)).convert('L')
-        f = BytesIO()
-        img.save(f, format="png", optimize=True, compress_level=9)
-        text = ocr_image(f)
-        if text:
-            menu = text.splitlines()
-    menu = pattern_slice(menu, [days_lower[today.weekday()]], days_lower + ["menü ár", "a menü mellé", "ajándék"])
+    mainbody = dom.xpath('/html/body//section[contains(@id, "mainbody")]//text()')
+    soup = "".join(mainbody)
+    start_date_match = re.search(r"(\d{4})\.(\d+)\.(\d+)\.", soup)
+
+    menu = []
+    if start_date_match:
+        year, month, day = start_date_match.groups()
+        start = date(int(year), int(month), int(day))
+        if start <= today.date() <= start + timedelta(days=6):
+            menu = pattern_slice(soup.splitlines(), [days_lower[today.weekday()]], days_lower + ["chef", "ajánlat"])
     return menu
 
 plugin = EbedkePlugin(
