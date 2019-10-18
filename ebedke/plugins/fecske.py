e06cd55ceded25f6ae4286dfee8efc8c35a94bb9
János Illés <ijanos@gmail.com>
2019-10-18 21:28:23 +0200

    annotate types

--- ebedke/plugins/fecske.py
+++ ebedke/plugins/fecske.py
@@ -1,5 +1,6 @@
 import re
-from datetime import timedelta, date
+from typing import List
+from datetime import timedelta, date, datetime
 from ebedke.utils.date import parse_hungarian_month
 from ebedke.utils.utils import on_workdays
 from ebedke.utils.http import get_dom
@@ -9,26 +10,24 @@ from ebedke.pluginmanager import EbedkePlugin
 URL = "http://www.fecskepresszo.com/heti-menu"
 
 @on_workdays
-def get_menu(today):
+def get_menu(today: datetime) -> List[str]:
     dom = get_dom(URL, force_utf8=True)
     menu_date = " ".join(txt.strip() for txt in dom.xpath('/html/body//div[@id="hetimenu_idopont"]//text()'))
     date_match = re.search(r"(\w+)\s(\d+)\W+(\w+)\s(\d+)", menu_date)
     if date_match:
         m1, d1, m2, d2 = date_match.groups()
-        m1 = parse_hungarian_month(m1)
-        m2 = parse_hungarian_month(m2)
-        d1 = int(d1)
-        d2 = int(d2)
+        month1 = parse_hungarian_month(m1)
+        month2 = parse_hungarian_month(m2)
         year1 = today.year
         year2 = today.year
-        if m2 < m1:
-            if today.month == m2:
+        if month2 < month1:
+            if today.month == month2:
                 year1 = today.year - 1
             year2 = year1 + 1
-        menu_start = date(year1, m1, d1)
-        menu_end = date(year2, m2, d2)
+        menu_start = date(year1, month1, int(d1))
+        menu_end = date(year2, month2, int(d2))
         if menu_start <= today.date() <= menu_end:
-            menu = dom.xpath('/html/body//div[@id="hetimenu_menu"]//span[@class="definition"]//text()')
+            menu: List[str] = dom.xpath('/html/body//div[@id="hetimenu_menu"]//span[@class="definition"]//text()')
             return menu
 
     return []
