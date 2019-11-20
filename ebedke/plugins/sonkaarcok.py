d3b9f6a83e43edab72babf23757cbe657d95208b
János Illés <ijanos@gmail.com>
2019-11-20 21:32:56 +0100

    add Sonkaarcok

---
+++ ebedke/plugins/sonkaarcok.py
@@ -0,0 +1,23 @@
+from datetime import timedelta
+from ebedke.utils.utils import on_workdays
+from ebedke.pluginmanager import EbedkePlugin
+
+
+FB_PAGE = "https://www.facebook.com/pg/sonkaarcok/posts"
+FB_ID = "189659631174505"
+
+@on_workdays
+def getMenu(_today):
+    return []
+
+plugin = EbedkePlugin(
+    enabled=True,
+    name='SonkaArcok',
+    id='ska',
+    url=FB_PAGE,
+    downloader=getMenu,
+    ttl=timedelta(hours=23),
+    cards=[],
+    groups=["ferenciek"],
+    coord=(47.490826, 19.059251)
+)
