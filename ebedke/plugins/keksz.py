4d10625447592dbff202cf326e2c2c706721dde1
János Illés <ijanos@gmail.com>
2019-11-20 21:07:40 +0100

    add Keksz plugin

---
+++ ebedke/plugins/keksz.py
@@ -0,0 +1,23 @@
+from datetime import timedelta
+from ebedke.utils.utils import on_workdays
+from ebedke.pluginmanager import EbedkePlugin
+
+
+FB_PAGE = "https://www.facebook.com/pg/kekszbudapest/posts/"
+FB_ID = "268952226502652"
+
+@on_workdays
+def getMenu(_today):
+    return []
+
+plugin = EbedkePlugin(
+    enabled=True,
+    name='Keksz',
+    id='kksz',
+    url=FB_PAGE,
+    downloader=getMenu,
+    ttl=timedelta(hours=23),
+    cards=[],
+    groups=["ferenciek"],
+    coord=(47.497596, 19.057130)
+)
