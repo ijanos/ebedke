cd8a62c0293107eec860c461a79004397ad8e64d
János Illés <ijanos@gmail.com>
2020-02-25 20:58:00 +0100

    switch Jedermann over to facebook based menu

--- ebedke/plugins/jedermann.py
+++ ebedke/plugins/jedermann.py
@@ -1,35 +1,18 @@
-from datetime import timedelta, date
-from re import split
-from ebedke.utils.date import on_workdays, days_lower
-from ebedke.utils.text import pattern_slice
-from ebedke.utils.http import get_dom
-from ebedke.utils.date import parse_hungarian_month
+from datetime import timedelta
 from ebedke.pluginmanager import EbedkePlugin
 
-
 URL = "http://www.jedermann.hu/#napi"
+FB_URL = "https://www.facebook.com/pg/jedermann.budapest/posts/"
 
-
-@on_workdays
-def get_menu(today):
-    dom = get_dom(URL, force_utf8=True)
-    menudate = ''.join(dom.xpath('/html/body//div[@id="datum"]//text()'))
-    start_month, start_day, *_ = split(" |-", menudate)
-    menu_start_date = date(today.year, parse_hungarian_month(start_month), int(start_day))
-    if menu_start_date <= today.date() < menu_start_date + timedelta(days=7):
-        menu = dom.xpath('/html/body//div[preceding-sibling::div[@id="datum"]]//article[@class="lmenu"]//text()')
-        menu = pattern_slice(menu, [days_lower[today.weekday()]], days_lower)
-    else:
-        menu = []
-    return menu
-
+def get_menu(_today):
+    return []
 
 plugin = EbedkePlugin(
     enabled=True,
     groups=["corvin"],
     name="Jedermann",
     id="jdr",
-    url=URL,
+    url=FB_URL,
     downloader=get_menu,
     ttl=timedelta(hours=24),
     cards=[],
