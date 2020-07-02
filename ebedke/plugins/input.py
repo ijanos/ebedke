85b0abaa24839cb92983c582d77bd610c3c3411f
János Illés <ijanos@gmail.com>
2020-07-02 19:21:47 +0200

    Use facebook for the Input bar menu

--- ebedke/plugins/input.py
+++ ebedke/plugins/input.py
@@ -1,35 +1,20 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 from ebedke.utils.date import on_workdays
-from ebedke.utils.text import pattern_slice
-from ebedke.utils.http import get_dom
 from ebedke.pluginmanager import EbedkePlugin
 
-FB_PAGE = "https://www.facebook.com/inputbistro/posts"
-FB_ID = "339892963137631"
+FB_PAGE = "https://www.facebook.com/inputkitchenandbar"
 URL = "https://www.input.hu/?page_id=1981"
 
 @on_workdays
-def get_menu(today):
-    dom = get_dom(URL)
-    page = dom.xpath("/html/body//div[@id='heti-men-page']//text()")
-    looks_like_a_date = lambda text: text.count(".") > 2 and str(today.year) in text
-    date = next(filter(looks_like_a_date, page))
-    date = datetime.strptime(date.split("-")[0].strip(" ."), "%Y.%m.%d")
-
-    if date <= today < date + timedelta(days=6):
-        menu = page
-    else:
-        menu = []
-
-    menu = pattern_slice(menu, ["levesek"], ["étlap", "function", "getElementById"])
-    return menu
+def get_menu(_):
+    return []
 
 
 plugin = EbedkePlugin(
     enabled=True,
     name='Input bar',
     id='ib',
-    url=URL,
+    url=FB_PAGE,
     downloader=get_menu,
     ttl=timedelta(hours=24),
     cards=["szep"],
