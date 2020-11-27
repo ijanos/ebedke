3b86d2b8c82f90e969795b4eacb690b06d355d9f
János Illés <ijanos@gmail.com>
2020-11-27 11:28:06 +0100

    remove cpython specific comparison

--- ebedke/plugins/keg.py
+++ ebedke/plugins/keg.py
@@ -12,7 +12,7 @@ def getMenu(today):
     this_year = today.strftime("%Y")
     menu_date = dom.xpath(f"/html/body//span[contains(text(), '{this_year}')]/text()")
     menu_date = menu_date[0].split('-')[0] if menu_date else -1
-    if menu_date is not -1 and dt.strptime(menu_date, "%Y.%m.%d").date() > today.date() - timedelta(days=6):
+    if menu_date != -1 and dt.strptime(menu_date, "%Y.%m.%d").date() > today.date() - timedelta(days=6):
         menu = list(skip_empty_lines([p.text_content() for p in dom.xpath('/html/body//div[@id="comp-jhonyf7y"]//p')]))
         if any(days_lower[today.weekday()] in line for line in menu):
             menu = list(pattern_slice(menu, [days_lower[today.weekday()]], days_lower, inclusive=False))
